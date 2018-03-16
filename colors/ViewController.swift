c08b910416ae93bf2cf2499062747ac74f066d80
Tiara Jarrett <tiarajarrett@tiaras-macbook-pro.local>
2018-03-16 13:22:50 -0500

    Initial Commit

---
+++ colors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  colors
+//
+//  Created by Tiara Jarrett on 3/16/18.
+//  Copyright © 2018 Tiara Jarrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
